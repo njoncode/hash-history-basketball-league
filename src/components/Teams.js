51276265446bb412bcd2ddd36f53a92ed8075e4b
Neha Joshi <neha@njoncode.local>
2021-03-09 13:39:22 +0530

    Project - Fetching the teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import { getTeamNames } from '../api'
+import { teams } from '../dummy-data'
 
-export default function Teams () {
+export default function Teams (props) {
+
+    const [teamNames, setTeamNames] = React.useState([])
+    const[loading, setLoading] = React.useState(true)
+
+    React.useEffect (() => {
+        getTeamNames()
+        .then((teamNames) => 
+            setTeamNames(teamNames),
+            setLoading(false)
+        )
+    })
+
+    const { location, match } = props
+   
     return (
         <div className='container two-column'>
-            TEAMS
+            <Sidebar
+                loading={loading}
+                title='Teams'
+                list={teamNames}
+                {...props}
+            />
+
+            {loading === false && location.pathname === '/teams' 
+                ? <div className='sidebar-instruction'>Select a Team</div>
+                : null}
         </div>
     )
 }
+
+
+/**
+ *          ...props   (all of our props, specifically our router props)
+ 
+ * If we are at /teams, if the user hasn't already selected a team & we are not loading, we want to show 'Select a Team'
+ 
+              {loading === false && location.pathname === '/teams' 
+            ? <div className='sidebar-instruction'>Select a Team</div>
+            : null}
+
+ */
+
